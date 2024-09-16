ee7382fe20e075de2aa56e1cfdd86a9d7e6dcc9c
coffinggx <coffinggx@gmail.com>
2024-09-16 17:23:03 +0545

    New Blog added

--- app/page.jsx
+++ app/page.jsx
@@ -27,13 +27,13 @@ const item = {
 export default function Home() {
   return (
     <motion.div
-      className="w-full h-screen flex items-center justify-center overflow-hidden" // Added overflow-hidden
+      className="w-full h-screen flex-col items-center justify-center overflow-hidden" // Added overflow-hidden
       variants={container}
       initial="hidden"
       animate="visible"
     >
-      <div className="flex md:flex-row flex-col items-center gap-10 h-full">
-        <motion.div variants={item} className="md:w-1/3 bg-white shadow rounded-lg px-8 py-10">
+      <div className="flex md:flex-row flex-col items-center gap-10 ">
+        <motion.div variants={item} className="md:w-1/3 shadow rounded-lg px-8 py-10">
           {/* Content */}
           <h2 className="text-3xl font-bold text-gray-800">Sudip Paudel</h2>
           <p className="text-lg text-gray-600">Based in Baglung, Nepal</p>
@@ -42,11 +42,21 @@ export default function Home() {
         <motion.div variants={item} className="md:w-1/3">
           <Image src={profile} alt="Sudip Paudel" width={200} height={200} className="rounded-full" />
         </motion.div>
-        <motion.div variants={item} className="md:w-1/3 bg-white shadow rounded-lg px-8 py-10">
+        <motion.div variants={item} className="md:w-1/3  shadow rounded-lg px-8 py-10">
           {/* Content */}
           <h2 className="text-3xl font-bold text-gray-800">FullStack Developer and Rust Developer</h2>
         </motion.div>
       </div>
+      <div className="flex justify-center items-center">
+        <motion.div 
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        className="w-[100px] h-[50px] p-3 mt-10  rounded-3xl bg-orange-400 cursor-pointer" >
+        Contact us
+      </motion.div>
+      </div>
+      
     </motion.div>
   );
 }
